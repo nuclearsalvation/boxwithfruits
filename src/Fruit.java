965286b9029cd005f889b6b1fa6b312824381488
Anton Golubev <xiagenius@gmail.com>
2024-03-18 14:35:17 +0300

    fruit class

---
+++ src/Fruit.java
@@ -0,0 +1,12 @@
+public abstract class Fruit {
+
+    private float weight;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
